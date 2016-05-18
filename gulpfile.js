e7dd4e6028f027d7cfc79109b6727b90eb8c7e6c
jverdaguer <jaime.verdaguer@gmail.com>
2016-05-18 14:00:46 -0400

    Added Coffee Scrtip files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const coffee = require('gulp-coffee');
+const coffeeSources = ['components/coffee/tagline.coffee'];
 
-gulp.task('log', function() {
-  gutil.log('Running First gulp file');
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });

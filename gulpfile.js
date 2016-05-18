f20dafe26c53744059c0765e467fc319c0fdfabd
jverdaguer <jaime.verdaguer@gmail.com>
2016-05-18 14:20:20 -0400

    Concat js files to build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const coffee = require('gulp-coffee');
+const concat = require('gulp-concat');
+
 const coffeeSources = ['components/coffee/tagline.coffee'];
+const jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js',
+];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
